298e94c3c293810ada724d0f80d7a7c97266306e
Siti087 <97527716+Siti087@users.noreply.github.com>
2022-01-12 00:06:14 +0700

    Update monero.sh

--- monero.sh
+++ monero.sh
@@ -1,5 +1,5 @@
 #monero
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
-tar xf xmrig-6.16.2-linux-x64.tar.gz
-./xmrig -a rx/0 -o monero.herominers.com:10191 -u 86sDXQ94AGQ2b88sAWmyK5Ad1j6zSCBCVReCJMjzvG94Z46XvuHcsyFTaeAPAjCb1JfqXV9An4RBeHc1g4HDZizc5RpPuaY -k --cpu 4 -p Cryptonian
+https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMzZAqZJPwGBCrDXMhHcBdruhXittumfRE.math -p x --cpu 4
